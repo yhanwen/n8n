03af56b7b9adebddd40e7af868aba19b8a272e72
Mutasem <mutdmour@gmail.com>
2021-08-30 16:57:43 +0200

    address design comments

--- packages/design-system/src/components/N8nInputLabel/InputLabel.vue
+++ packages/design-system/src/components/N8nInputLabel/InputLabel.vue
@@ -2,10 +2,14 @@
 	<div :class="$style.inputLabel">
 		<label>
 			<div :class="$style.label">
-				<span>{{ props.label }}</span>
-				<span v-if="props.tooltipText" :class="$style.infoIcon">
-					<n8n-tooltip :content="props.tooltipText" placement="top">
+				<span>
+					{{ props.label }}
+					<span v-if="props.required" :class="$style.required">*</span>
+				</span>
+				<span :class="$style.infoIcon" v-if="props.tooltipText">
+					<n8n-tooltip placement="top" :popper-class="$style.tooltipPopper">
 						<n8n-icon icon="info-circle" />
+						<div slot="content" v-html="props.tooltipText"></div>
 					</n8n-tooltip>
 				</span>
 			</div>
@@ -53,5 +57,13 @@ export default {
 .infoIcon {
 	color: var(--color-text-light);
 	display: var(--info-icon-display, none);
+}
+
+.required {
+	color: var(--color-primary);
+}
+
+.tooltipPopper {
+	max-width: 400px;
 }
 </style>
